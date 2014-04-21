f524fdc774e1010a746c43f8c7b0e213b0e5dfce
Brendan Reville <brendan@code.org>
2014-04-21 10:56:39 -0700

    Initial work on per-course landing pages.

--- app/models/script.rb
+++ app/models/script.rb
@@ -21,10 +21,15 @@ class Script < ActiveRecord::Base
     @@hoc_script ||= Script.includes(script_levels: { level: [:game, :concepts] }).find(HOC_ID)
   end
 
+ def self.flappy_script
+    @@flappy_script ||= Script.includes(script_levels: { level: [:game, :concepts] }).find(JIGSAW_ID)
+  end
+
   def self.get_from_cache(id)
     case id
       when TWENTY_HOUR_ID then twenty_hour_script
       when HOC_ID then hoc_script
+      when FLAPPY_ID then flappy_script
       else Script.includes(script_levels: { level: [:game, :concepts] }).find(id)
     end
   end
