4c21a4cdf7dc7a73228fc917f36ba44bece13026
Tobias Kahan <tobyk100@gmail.com>
2014-04-24 16:52:31 -0700

    Refactoring.

--- test/models/level_test.rb
+++ test/models/level_test.rb
@@ -3,10 +3,12 @@ include ActionDispatch::TestProcess
 
 class LevelTest < ActiveSupport::TestCase
   setup do
-    @custom_data = {:game_id=>23, "user_id" => 1, :name=>"__bob4", :level_num=>"custom", :skin=>"artist", :solution_level_source_id=>4, :user_id=>1, :instructions=>"sdfdfs"}
-    @turtle_data = {:game_id=>23, :name=>"__bob4", :level_num=>"custom", :skin=>"artist", :solution_level_source_id=>4, :user_id=>1, :instructions=>"sdfdfs", :program=>"<hey>"}
-    @maze_data = {:game_id=>25, :name=>"__bob4", :level_num=>"custom", :skin=>"birds", :user_id=>1, :instructions=>"sdfdfs"}
-    @custom_level = Level.create(@custom_data)
+    @turtle_data = {:game_id=>23, :name=>"__bob4", :level_num=>"custom", :skin=>"artist", :instructions=>"sdfdfs"}
+    @custom_turtle_data = @turtle_data.merge(:solution_level_source_id=>4, :user_id=>1, :program=>"<hey>")
+    @maze_data = {:game_id=>25, :name=>"__bob4", :level_num=>"custom", :skin=>"birds", :instructions=>"sdfdfs"}
+    @custom_maze_data = @maze_data.merge(:user_id=>1)
+
+    @custom_level = Level.create(@custom_maze_data)
     @level = Level.create(@maze_data)
   end
 
@@ -37,7 +39,7 @@ class LevelTest < ActiveSupport::TestCase
 
   test "cannot create two custom levels with same name" do
     assert_no_difference('Level.count') do
-      level2 = Level.create(@custom_data)
+      level2 = Level.create(@custom_maze_data)
       assert_not level2.valid?
       assert level2.errors.include?(:name)
     end
@@ -45,8 +47,8 @@ class LevelTest < ActiveSupport::TestCase
 
   test "can create two custom levels with different names" do
     assert_difference('Level.count', 1) do
-      @custom_data[:name] = "__swoop"
-      level2 = Level.create(@custom_data)
+      @custom_maze_data[:name] = "__swoop"
+      level2 = Level.create(@custom_maze_data)
       assert level2.valid?
     end
   end
@@ -57,18 +59,19 @@ class LevelTest < ActiveSupport::TestCase
   end
 
   test "create turtle level of correct subclass" do
-    level = Turtle.create_from_level_builder(@turtle_data)
+    level = Turtle.create(@turtle_data)
     assert_equal "Turtle", level.type
   end
 
   test "create maze level of correct subclass" do
-    level = Maze.create_from_level_builder(@maze_data)
+    level = Maze.create(@maze_data)
     assert_equal "Maze", level.type
   end
 
   test "create turtle level from level builder" do
-    level = Turtle.create_from_level_builder(@turtle_data)
+    level = Turtle.create_from_level_builder(@custom_turtle_data)
 
-    assert_equal level.instructions, @turtle_data[:instructions]
+    assert_equal "Turtle", level.type
+    assert_equal level.instructions, @custom_turtle_data[:instructions]
   end
 end
