9a97cd1c394f71b766866f16c0f3eaeee8c203e5
deploy-code-org <dev@code.org>
2014-04-25 00:02:27 +0000

    Automatically built.

--- public/blockly-dist/js/hi_in/studio.js
+++ public/blockly-dist/js/hi_in/studio.js
@@ -4783,6 +4783,17 @@ exports.mod = function(number, mod) {
   return ((number % mod) + mod) % mod;
 };
 
+/**
+ * Generates an array of integers from start to end inclusive
+ */
+exports.range = function(start, end) {
+  var ints = [];
+  for (var i = start; i <= end; i++) {
+    ints.push(i);
+  }
+  return ints;
+};
+
 },{}],33:[function(require,module,exports){
 // Serializes an XML DOM node to a string.
 exports.serialize = function(node) {
