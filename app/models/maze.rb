a290429b76d52e78b75a3274eb7931dc62ec9eb3
Tobias Kahan <tobyk100@gmail.com>
2014-04-25 13:55:29 -0700

    Add Karel type and refactor.

--- app/models/maze.rb
+++ app/models/maze.rb
@@ -11,7 +11,7 @@ class Maze < Level
     game = Game.custom_maze
     size = params[:size].to_i
 
-    maze = Level.parse_maze(contents, params[:type], size)
+    maze = parse_maze(contents, size)
 
     skin = params[:type] == 'maze' ? 'birds' : 'farmer'
     level = create(level_params.merge(game: game, level_num: 'custom', skin: skin))
@@ -20,24 +20,44 @@ class Maze < Level
     level
   end
 
+  # contents - should respond to read by returning a 2d square array
+  #   with the given size, representing a blockly level.
+  # Throws ArgumentError if there is a non integer value in the array.
+  def self.read_and_convert_maze_to_integer(contents, size)
+    raw_maze = contents.read[0...size]
+    raw_maze.map {|row| row.map {|cell| Integer(cell)}}
+  end
+
+  # Parses the 2d array contents.
+  # If type is "maze" return a single entry hash with 'maze' mapping to a 2d
+  #   array that Blockly can render.
+  # If type is "karel" return a 3 entry hash with keys 'maze', 'initial_dirt',
+  #   and 'final_dirt', the keys map to 2d arrays blockly can render.
+  # Throws ArgumentError if there is a non integer value in the array.
+  def self.parse_maze(contents, size)
+    { 'maze' => read_and_convert_maze_to_integer(contents, size) }
+  end
+
+  def common_blocks
+    '<block type="maze_moveForward"></block>
+    <block type="maze_turn">
+      <title name="DIR">turnLeft</title>
+    </block>
+    <block type="maze_turn">
+      <title name="DIR">turnRight</title>
+    </block>
+    <block type="controls_repeat">
+      <title name="TIMES">5</title>
+    </block>'
+  end
+
   def complete_toolbox
-    '<xml id="toolbox" style="display: none;">
-      <block type="maze_moveForward"></block>
-      <block type="maze_turn">
-        <title name="DIR">turnLeft</title>
-      </block>
-      <block type="maze_turn">
-        <title name="DIR">turnRight</title>
-      </block>
-      <block type="maze_forever"></block>
+      common_blocks + '<block type="maze_forever"></block>
       <block type="maze_if">
         <title name="DIR">isPathLeft</title>
       </block>
       <block type="maze_if"></block>
       <block type="maze_ifElse"></block>
-      <block type="controls_repeat">
-        <title name="TIMES">5</title>
-      </block>
       <block type="maze_forever"></block>
       <block type="maze_if">
         <title name="DIR">isPathLeft</title>
@@ -45,7 +65,6 @@ class Maze < Level
       <block type="maze_if">
         <title name="DIR">isPathRight</title>
       </block>
-      <block type="maze_ifElse"></block>
-    </xml>'
+      <block type="maze_ifElse"></block>'
   end
 end
--- app/models/turtle.rb
+++ app/models/turtle.rb
@@ -12,84 +12,82 @@ class Turtle < Level
     level
   end
 
-  def complete_toolbox
-    '<xml id="toolbox" style="display: none;">
-      <category id="actions" name="Actions">
-        <block type="draw_move">
-          <value name="VALUE">
-            <block type="math_number">
-              <title name="NUM">100</title>
-            </block>
-          </value>
-        </block>
-        <block type="draw_turn">
-          <value name="VALUE">
-            <block type="math_number">
-              <title name="NUM">90</title>
-            </block>
-          </value>
-        </block>
-        <block id="draw-width" type="draw_width">
-          <value name="WIDTH">
-            <block type="math_number">
-              <title name="NUM">1</title>
-            </block>
-          </value>
-        </block>
-      </category>
-      <category name="Color">
-        <block id="draw-color" type="draw_colour">
-          <value name="COLOUR">
-            <block type="colour_picker"></block>
-          </value>
-        </block>
-        <block id="draw-color" type="draw_colour">
-          <value name="COLOUR">
-            <block type="colour_random"></block>
-          </value>
-        </block>
-      </category>
-      <category name="Functions" custom="PROCEDURE"></category>
-      <category name="Loops">
-        <block type="controls_for_counter">
-          <value name="FROM">
-            <block type="math_number">
-              <title name="NUM">1</title>
-            </block>
-          </value>
-          <value name="TO">
-            <block type="math_number">
-              <title name="NUM">100</title>
-            </block>
-          </value>
-          <value name="BY">
-            <block type="math_number">
-              <title name="NUM">10</title>
-            </block>
-          </value>
-        </block>
-        <block type="controls_repeat">
-          <title name="TIMES">4</title>
-        </block>
-      </category>
-      <category name="Math">
-        <block type="math_number"></block>
-        <block type="math_arithmetic" inline="true"></block>
-        <block type="math_random_int">
-          <value name="FROM">
-            <block type="math_number">
-              <title name="NUM">1</title>
-            </block>
-          </value>
-          <value name="TO">
-            <block type="math_number">
-              <title name="NUM">100</title>
-            </block>
-          </value>
-        </block>
-        <block type="math_random_float"></block>
-      </category>
-      <category name="Variables" custom="VARIABLE"></category>
-    </xml>'
+  def toolbox
+    '<category id="actions" name="Actions">
+      <block type="draw_move">
+        <value name="VALUE">
+          <block type="math_number">
+            <title name="NUM">100</title>
+          </block>
+        </value>
+      </block>
+      <block type="draw_turn">
+        <value name="VALUE">
+          <block type="math_number">
+            <title name="NUM">90</title>
+          </block>
+        </value>
+      </block>
+      <block id="draw-width" type="draw_width">
+        <value name="WIDTH">
+          <block type="math_number">
+            <title name="NUM">1</title>
+          </block>
+        </value>
+      </block>
+    </category>
+    <category name="Color">
+      <block id="draw-color" type="draw_colour">
+        <value name="COLOUR">
+          <block type="colour_picker"></block>
+        </value>
+      </block>
+      <block id="draw-color" type="draw_colour">
+        <value name="COLOUR">
+          <block type="colour_random"></block>
+        </value>
+      </block>
+    </category>
+    <category name="Functions" custom="PROCEDURE"></category>
+    <category name="Loops">
+      <block type="controls_for_counter">
+        <value name="FROM">
+          <block type="math_number">
+            <title name="NUM">1</title>
+          </block>
+        </value>
+        <value name="TO">
+          <block type="math_number">
+            <title name="NUM">100</title>
+          </block>
+        </value>
+        <value name="BY">
+          <block type="math_number">
+            <title name="NUM">10</title>
+          </block>
+        </value>
+      </block>
+      <block type="controls_repeat">
+        <title name="TIMES">4</title>
+      </block>
+    </category>
+    <category name="Math">
+      <block type="math_number"></block>
+      <block type="math_arithmetic" inline="true"></block>
+      <block type="math_random_int">
+        <value name="FROM">
+          <block type="math_number">
+            <title name="NUM">1</title>
+          </block>
+        </value>
+        <value name="TO">
+          <block type="math_number">
+            <title name="NUM">100</title>
+          </block>
+        </value>
+      </block>
+      <block type="math_random_float"></block>
+    </category>
+    <category name="Variables" custom="VARIABLE"></category>'
   end
 end
