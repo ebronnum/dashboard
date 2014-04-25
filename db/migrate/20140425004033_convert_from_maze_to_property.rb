b0882fe31aac2316350196b8278bde011a1487e9
Tobias Kahan <tobyk100@gmail.com>
2014-04-24 18:00:35 -0700

    Fix migration

--- db/migrate/20140425004033_convert_from_maze_to_property.rb
+++ db/migrate/20140425004033_convert_from_maze_to_property.rb
@@ -2,7 +2,7 @@ class ConvertFromMazeToProperty < ActiveRecord::Migration
   def change
     Level.all.each do |level|
       if !level.maze.nil?
-        level.property.update(maze: JSON.parse(level.maze))
+        level.properties.update(maze: JSON.parse(level.maze))
         level.save!
       end
     end
