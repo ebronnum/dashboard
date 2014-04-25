554d4009e91f1ef7fc214d3d6bbb42e5ee76ef48
Will Jordan <will@code.org>
2014-04-24 18:02:25 -0700

    Refactoring seed.rake task for custom levels. Moved legacy Script TSV files into YAML bundled with metadata.

--- app/models/concept.rb
+++ app/models/concept.rb
@@ -1,4 +1,8 @@
+# A Concept contains a set of Levels
+# A Video can be associated with a Concept
+# Trophies are awarded based on percentage completion of Concepts
 class Concept < ActiveRecord::Base
+  include Seeded
   has_and_belongs_to_many :levels
   belongs_to :video
   # Can't call static from filter. Leaving in place for fixing later
@@ -10,5 +14,14 @@ class Concept < ActiveRecord::Base
 
   def self.expire_cache
     @@all_cache = nil
+  end
+
+  def self.setup
+    transaction do
+      reset_db
+      %w(sequence if if_else loop_times loop_until loop_while loop_for function parameters).each_with_index do |concept, id|
+        Concept.create!(id: id + 1, name: concept, video: Video.find_by_key(concept))
+      end
+    end
   end
 end
