d8a65bfa85392c7a894bea27a131e7c85cdf0226
Will Jordan <will@code.org>
2014-04-29 19:43:36 -0700

    symbolize keys in script. initial work on script model test

--- test/models/script_test.rb
+++ test/models/script_test.rb
@@ -4,4 +4,12 @@ class ScriptTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'create script from script text' do
+    Game.create!(name: 'test game')
+    script = Script.add_script(
+      {name: 'test script'},
+      [{game: 'test game', name: 'test level', level_num: 'test_level'}]
+    )
+    puts "script: #{script}"
+  end
 end
