24ccb9df6bbb0c25dcc494114f0e72bb807b52e8
Tobias Kahan <tobyk100@gmail.com>
2014-04-30 12:19:58 -0700

    Moved test from controller to helper.

---
+++ test/helpers/levels_helper_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class LevelsHelperTest < ActionView::TestCase
+  include LocaleHelper
+
+  setup do
+    @maze_data = {:game_id=>25, :user_id => 1, :name=>"__bob4", :level_num=>"custom", :skin=>"birds", :instructions=>"sdfdfs"}
+    @level = Maze.create(@maze_data)
+  end
+
+  test "should parse maze level with non string array" do
+    @level.properties["maze"] = [[0, 0], [2, 3]]
+    level, options = blockly_options
+    assert (level["maze"].is_a? Array), "Maze is not an array"
+
+    @level.properties["maze"] = @level.properties["maze"].to_s
+    level, options = blockly_options
+    assert (level["maze"].is_a? Array), "Maze is not an array"
+  end
+end
