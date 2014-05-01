6daedaac4b07fb59483efca647381218a09e2665
Will Jordan <will@code.org>
2014-04-30 17:11:22 -0700

    refactor seed:games

--- app/models/game.rb
+++ app/models/game.rb
@@ -43,6 +43,7 @@ class Game < ActiveRecord::Base
         CustomMaze:maze
         Studio:studio
         Jigsaw:jigsaw
+        MazeStep:maze
       ).each_with_index do |game, id|
         name, app, intro_video = game.split ':'
         Game.create!(id: id + 1, name: name, app: app, intro_video: Video.find_by_key(intro_video))
