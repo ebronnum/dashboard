157a82b12e609d8815b8a6e6e8850ab765aa23f0
Will Jordan <will@code.org>
2014-05-01 13:38:24 -0700

    fix bug with stale instructions after locale switch (w/tests)

--- test/helpers/levels_helper_test.rb
+++ test/helpers/levels_helper_test.rb
@@ -17,4 +17,25 @@ class LevelsHelperTest < ActionView::TestCase
     level, options = blockly_options
     assert (level["map"].is_a? Array), "Maze is not an array"
   end
+
+  test "change default level localization after locale switch" do
+    DEFAULT_LOCALE = 'en-us'
+    NEW_LOCALE = 'de-de'
+    @level.instructions = nil
+    @level.level_num = '2_2'
+    I18n.locale = DEFAULT_LOCALE
+    level, options = blockly_options
+    assert_equal I18n.t('data.level.instructions.maze_2_2', locale: DEFAULT_LOCALE), level['instructions']
+
+    I18n.locale = NEW_LOCALE
+    level, options = blockly_options
+    assert_equal I18n.t('data.level.instructions.maze_2_2', locale: NEW_LOCALE), level['instructions']
+  end
+
+  test "display custom level instructions instead of localized string" do
+    @level.instructions = 'custom instructions'
+    @level.level_num = '2_2'
+    level, options = blockly_options
+    assert_equal 'custom instructions', level['instructions']
+  end
 end
